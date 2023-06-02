b45ac57272cbe2aec3c651d244952e5b1fa53d66
Mark LaPointe <mark@cloudbsd.org>
2023-06-02 01:17:24 -0400

    Start platform specific properties object. Helps with the app icon, and how the size looks off in KDE

--- src/platform/windows/windows_font.py
+++ src/platform/windows/windows_font.py
@@ -1,5 +1,6 @@
-from src.platform.interfaces.font_interface import FontInterface
 import ctypes
+
+from src.platform.interfaces.font_interface import FontInterface
 
 
 class WindowsFont(FontInterface):
@@ -8,11 +9,10 @@ class WindowsFont(FontInterface):
         super().__init__()
 
     def add_font_resource(self, font_file: str) -> None:
-        if self.is_windows():
-            gdi32 = ctypes.WinDLL('gdi32')
-            gdi32.AddFontResourceW(font_file)
+        gdi32 = ctypes.WinDLL('gdi32')
+        gdi32.AddFontResourceW(font_file)
 
     def remove_font_resource(self, font_file: str) -> None:
-        if self.is_windows():
-            gdi32 = ctypes.WinDLL('gdi32')
-            gdi32.RemoveFontResourceW(font_file)
+        gdi32 = ctypes.WinDLL('gdi32')
+        gdi32.RemoveFontResourceW(font_file)
+
